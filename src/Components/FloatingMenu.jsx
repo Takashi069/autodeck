edd321f55a3cdd69a3ea1a7e46f2ef8062479479
Akash <akashharikumar2013@gmail.com>
2022-10-19 20:53:01 +0530

    Built Multi-Menu Floating Button

--- src/Components/FloatingMenu.jsx
+++ src/Components/FloatingMenu.jsx
@@ -1,12 +1,17 @@
 import './component.css'
-import { AiOutlineSearch } from "react-icons/ai";
-
+import { AiOutlineSearch,AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { BiCalculator, BiRupee } from "react-icons/bi";
+import { useState } from 'react';
 const FloatingMenu = ({toggleSearch, setToggleSearch}) => {
-    
+    const [toggleMenu,setToggleMenu] = useState(false)
     return ( 
-        <span className="floating-menu" onClick={()=>{setToggleSearch(!toggleSearch)}}>
-            <AiOutlineSearch size={50}/>
-        </span>
+        <div className='floating-menu flex flex-col p-3'>
+            {!toggleMenu && <AiOutlineMenu className='' size={35} onClick={()=>{setToggleMenu(!toggleMenu)}}/>}
+            <AiOutlineSearch className={toggleMenu ? "mb-4 button-animate":"hidden mb-4 button-animate"} size={35} onClick={()=>{setToggleSearch(!toggleSearch)}}/>
+            <BiRupee className={toggleMenu ? "mb-4 button-animate":"hidden mb-4 button-animate"} size={35}/>
+            <BiCalculator className={toggleMenu ? "mb-4 button-animate":"hidden mb-4 button-animate"} size={35}/>
+            {toggleMenu && <AiOutlineClose className='mb-4 button-animate' size={35} onClick={()=>{setToggleMenu(!toggleMenu)}}/>}
+        </div>  
      );
 }
  
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = ({toggleSearch}) => {
     },[toggleSearch])
     return ( 
         <div className="bg-slate-600 sticky w-[100%] p-3 navbar" >
-            <h1 className="text-left font-extrabold text-3xl mb-7">AUTODECK</h1>
+            <h1 className="text-left font-extrabold text-3xl mb-7 mt-3">AUTODECK</h1>
             {toggleSearch && <Search/>}
         </div>
      );
